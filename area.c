4036690da69348dc34f19955bd867551cc56344a
aryan gupta <168979682+aryan-gupta777@users.noreply.github.com>
2024-07-30 14:39:03 +0530

    chapter 1

--- area.c
+++ area.c
@@ -3,7 +3,7 @@ int main()
 {
     int a;
     printf("enter side of square ");
-    scanf("%d", &a);
+    scanf("%d", &a);//hello
     printf("area of square is %d", a*a);
     return 0;
 }
---
+++ peri.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+  printf("enter value of a  ");
+  scanf("%d", &a);
+
+  printf("enter value of b ");
+  scanf("%d", &b);
+   int c = (a+b)*2 ;
+  
+  printf("perimeter of rectange is %d", c);
+  return 0;
+
+
+}
